a795f7bd7bff69b5fbbf5a7a8432f76c81089777
NekroDarkmoon <nekrodarkmoon@gmail.com>
2021-12-14 05:12:51 -0800

    Refactored getData for bulkMenu

--- modules/bulkMenu.js
+++ modules/bulkMenu.js
@@ -30,11 +30,63 @@ export class BulkMenu extends Application {
 
 	getData(options = {}) {
 		// Get Directories
-		const data = {};
+		const docTypes = {
+			actors: game.actors.directory,
+			journals: game.journal.directory,
+			items: game.items.directory,
+			macros: game.macros.directory,
+			scenes: game.scenes.directory,
+			tables: game.tables.directory,
+			playlists: game.playlists.directory,
+		};
+
+		// Construct self folder lists
+		const directory = {};
+
+		for (const docType in docTypes) {
+			const folders = docTypes[docType].folders;
+
+			// Add to directory
+			directory[docType] = { folders: [], orphans: [] };
+
+			folders.forEach(folder => {
+				const temp = this.permsFilter(folder.content);
+
+				// Create our own object
+				const customFolder = {
+					id: folder.data._id,
+					name: folder.data.name,
+					content: temp,
+					type: folder.data.type,
+				};
+				directory[docType].folders.push(customFolder);
+			});
+
+			// Add content not in folder
+			const entities = docTypes[docType].documents;
+			const noParent = this.permsFilter(
+				entities.filter(e => e.data.folder === null)
+			);
+
+			directory[docType].orphans = [...noParent];
+		}
+
+		const data = directory;
+		console.info(directory);
 		return data;
 	}
 
 	activateListeners(html) {
 		super.activateListeners(html);
+	}
+
+	/**
+	 * @param {Array<Object>} inputArray
+	 * @returns {Array<Object>}
+	 */
+	permsFilter(inputArray) {
+		return inputArray.filter(
+			e => e.data.permission.default == 3 || e.data.permission[this.userID] == 3
+		);
 	}
 }

05950dd02d48ff1e581ef7cde0da831fb6ca6bc8
NekroDarkmoon <nekrodarkmoon@gmail.com>
2021-12-16 04:46:45 -0800

    Working code for actor search

--- modules/bulkMenu.js
+++ modules/bulkMenu.js
@@ -27,6 +27,12 @@ export class BulkMenu extends Application {
 			tabs: [
 				{ navSelector: '.tabs', contentSelector: 'form', intial: 'actors' },
 			],
+			filters: [
+				{
+					inputSelector: 'input[name="search"]',
+					contentSelector: '.bulk-display',
+				},
+			],
 		});
 	}
 
@@ -82,43 +88,61 @@ export class BulkMenu extends Application {
 
 	activateListeners($parent) {
 		super.activateListeners($parent);
-		const directory = this.directory;
-		const $section = $parent.find(`section[data-tab="actors"]`);
-		const $searchWrapper = $section.find(`.bm-search-wrapper`);
-		const $input = $searchWrapper.find(`input[type="text"]`);
-		const $fieldset = $section.find(`.bulk-display`);
-		const $resultBox = $section.find(`.bm-search-result-box`);
-		let displayArray;
-
-		// Hide resultBox
-		$resultBox.hide();
-
-		// Create a filter list
-		$input.on('keyup', e => {
-			const userInput = e.target.value.toLowerCase();
-
-			if (userInput) {
-				const searchAbleArray = [...directory.actors.orphans];
-				displayArray = searchAbleArray.filter(val =>
-					val.name.includes(userInput)
-				);
-
-				$resultBox.text(displayArray);
-				$searchWrapper.addClass('active').trigger('classChange');
-			} else $searchWrapper.removeClass('active').trigger('classChange');
-		});
-
-		// Change content based on search
-		$searchWrapper.on('classChange', e => {
-			if ($searchWrapper.hasClass('active')) {
-				$fieldset.hide();
-				$resultBox.show();
-			} else {
-				$resultBox.hide();
-				$fieldset.show();
-			}
-		});
 	}
+
+	_onSearchFilter(event, query, rgx, $parent) {
+		for (const $entity of $parent.querySelectorAll('.bm-entity')) {
+			if (!query) {
+				$entity.classList.remove('hiden');
+				continue;
+			}
+
+			const title = $entity.querySelector(`.bm-check`).dataset.name;
+			console.log(title);
+			const match = rgx.test(SearchFilter.cleanQuery(title));
+			$entity.classList.toggle('hidden', !match);
+		}
+	}
+
+	// activateListeners($parent) {
+	// 	super.activateListeners($parent);
+	// 	const directory = this.directory;
+	// 	const $section = $parent.find(`section[data-tab="actors"]`);
+	// 	const $searchWrapper = $section.find(`.bm-search-wrapper`);
+	// 	const $input = $searchWrapper.find(`input[type="text"]`);
+	// 	const $fieldset = $section.find(`.bulk-display`);
+	// 	const $resultBox = $section.find(`.bm-search-result-box`);
+	// 	let displayArray;
+
+	// 	// Hide resultBox
+	// 	$resultBox.hide();
+
+	// 	// Create a filter list
+	// 	$input.on('keyup', e => {
+	// 		const userInput = e.target.value.toLowerCase();
+
+	// 		if (userInput) {
+	// 			const searchAbleArray = [...directory.actors.orphans];
+	// 			displayArray = searchAbleArray.filter(val =>
+	// 				val.name.includes(userInput)
+	// 			);
+
+	// 			$resultBox.text(displayArray);
+	// 			$searchWrapper.addClass('active').trigger('classChange');
+	// 		} else $searchWrapper.removeClass('active').trigger('classChange');
+	// 	});
+
+	// 	// Change content based on search
+	// 	$searchWrapper.on('classChange', e => {
+	// 		if ($searchWrapper.hasClass('active')) {
+	// 			$fieldset.hide();
+	// 			$resultBox.show();
+	// 		} else {
+	// 			$resultBox.hide();
+	// 			$fieldset.show();
+	// 		}
+	// 	});
+	// }
 
 	/**
 	 * @param {Array<Object>} inputArray

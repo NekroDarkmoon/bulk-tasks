d092ae8b25c914101ce3d4fa94947a0bcc2793e9
NekroDarkmoon <nekrodarkmoon@gmail.com>
2021-12-17 08:33:29 -0800

    Implemented search #6

--- modules/bulkMenu.js
+++ modules/bulkMenu.js
@@ -103,46 +103,6 @@ export class BulkMenu extends Application {
 		}
 	}
 
-	// activateListeners($parent) {
-	// 	super.activateListeners($parent);
-	// 	const directory = this.directory;
-	// 	const $section = $parent.find(`section[data-tab="actors"]`);
-	// 	const $searchWrapper = $section.find(`.bm-search-wrapper`);
-	// 	const $input = $searchWrapper.find(`input[type="text"]`);
-	// 	const $fieldset = $section.find(`.bulk-display`);
-	// 	const $resultBox = $section.find(`.bm-search-result-box`);
-	// 	let displayArray;
-
-	// 	// Hide resultBox
-	// 	$resultBox.hide();
-
-	// 	// Create a filter list
-	// 	$input.on('keyup', e => {
-	// 		const userInput = e.target.value.toLowerCase();
-
-	// 		if (userInput) {
-	// 			const searchAbleArray = [...directory.actors.orphans];
-	// 			displayArray = searchAbleArray.filter(val =>
-	// 				val.name.includes(userInput)
-	// 			);
-
-	// 			$resultBox.text(displayArray);
-	// 			$searchWrapper.addClass('active').trigger('classChange');
-	// 		} else $searchWrapper.removeClass('active').trigger('classChange');
-	// 	});
-
-	// 	// Change content based on search
-	// 	$searchWrapper.on('classChange', e => {
-	// 		if ($searchWrapper.hasClass('active')) {
-	// 			$fieldset.hide();
-	// 			$resultBox.show();
-	// 		} else {
-	// 			$resultBox.hide();
-	// 			$fieldset.show();
-	// 		}
-	// 	});
-	// }
-
 	/**
 	 * @param {Array<Object>} inputArray
 	 * @returns {Array<Object>}

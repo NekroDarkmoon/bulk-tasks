43a32b72af47bca58fe25652354802fc9076f550
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-11-13 01:38:08 -0800

    Updates export functionality to preserve folder structure. Updates file name to replace '?' with '_'

--- modules/partials/export.js
+++ modules/partials/export.js
@@ -1,25 +1,60 @@
+function replaceName(name) {
+	return name.slugify().replace(/([?:\/])/, '_');
+}
+
 export function onExport(data) {
 	const zip = new JSZip();
-
 	const documents = [...data].map(doc => game[doc.type].get(doc.id));
+	console.log(documents);
 
-	// Create a file for each document
-	documents.forEach(d => {
-		// Create Json Object
-		const data = d.toCompendium(null);
-		data.flags['exportSource'] = {
-			world: game.world.id,
-			system: game.system.id,
-			coreVersion: game.version,
-			systemVersion: game.system.version,
-		};
+	const folders = documents.reduce((acc, doc) => {
+		let folder = doc.folder?._id;
+		if (!folder) folder = 'root';
 
-		// Add file to zip
-		zip.file(
-			`${d.name.slugify().replace(/([:\/])/, '_')}.json`,
-			JSON.stringify(data, null, 2)
-		);
-	});
+		acc[folder] ??= [];
+		acc[folder].push(doc);
+		return acc
+	}, {});
+	
+	// Check if there are any folders
+	if (Object.keys(folders).length > 0) {
+
+		Object.keys(folders).forEach(folder => {
+			let folderZip = zip;
+
+			if (folder !== 'root') {
+				const folderName = game.folders.get(folder).name;
+				folderZip = zip.folder(folderName.slugify());
+			}
+
+			const docs = folders[folder];
+			docs.forEach((doc) => {
+				const data = doc.toCompendium(null);
+				data.flags['exportSource'] = {
+					world: game.world.id,
+					system: game.system.id,
+					coreVersion: game.version,
+					systemVersion: game.system.version,
+				};
+
+				// Add file to zip
+				folderZip.file(`${replaceName(doc.name)}.json`, JSON.stringify(data, null, 2));
+			});
+		});
+	} else {
+		documents.forEach((doc) => {
+			const data = doc.toCompendium(null);
+			data.flags['exportSource'] = {
+				world: game.world.id,
+				system: game.system.id,
+				coreVersion: game.version,
+				systemVersion: game.system.version,
+			};
+
+			// Add file to zip
+			zip.file(`${replaceName(doc.name)}.json`, JSON.stringify(data, null, 2));
+		});
+	}
 
 	// Download Zip
 	console.log('Generating Zip');

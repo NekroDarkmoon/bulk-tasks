3ff43b8d0d8cce151e4d02cc0c93cab2237f48bb
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2025-06-18 20:40:43 -0700

    Updates hook and parent selector for button

--- src/bulkTasks.ts
+++ src/bulkTasks.ts
@@ -1,47 +1,47 @@
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                      Imports
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-import './scss/main.scss';
+import "./scss/main.scss";
 
-import { moduleId, moduleName } from './constants.ts';
-import { registerSettings } from './settings.ts';
-import { addBulkButton } from './utils/addBulkButton.ts';
-import { BulkTaskDialog } from './dialogs/BulkTasks.svelte.ts';
+import { moduleId, moduleName } from "./constants.ts";
+import { registerSettings } from "./settings.ts";
+import { addBulkButton } from "./utils/addBulkButton.ts";
+import { BulkTaskDialog } from "./dialogs/BulkTasks.svelte.ts";
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                     Type Helpers
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 declare global {
-	interface AssumeHookRan {
-		init: never;
-	}
+  interface AssumeHookRan {
+    init: never;
+  }
 
-	interface AssumeHookRan {
-		setup: never;
-	}
+  interface AssumeHookRan {
+    setup: never;
+  }
 
-	interface AssumeHookRan {
-		ready: never;
-	}
+  interface AssumeHookRan {
+    ready: never;
+  }
 }
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                        Main
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-Hooks.on('init', () => {
-	registerSettings();
-	console.log(`${moduleName} | Initialized.`);
+Hooks.on("init", () => {
+  registerSettings();
+  console.log(`${moduleName} | Initialized.`);
 });
 
-Hooks.on('renderSidebarTab', async (app, html) => {
-	addBulkButton(app, html);
+Hooks.on("renderAbstractSidebarTab", async (app, html) => {
+  addBulkButton(app, html);
 });
 
-Hooks.on('setup', async () => {
-	console.log(`${moduleName} | Setup Complete.`);
+Hooks.on("setup", async () => {
+  console.log(`${moduleName} | Setup Complete.`);
 });
 
-Hooks.on('ready', async () => {
-	console.log(`${moduleName} | Ready.`);
-	// new BulkTaskDialog().render(true);
+Hooks.on("ready", async () => {
+  console.log(`${moduleName} | Ready.`);
+  // new BulkTaskDialog().render(true);
 });
